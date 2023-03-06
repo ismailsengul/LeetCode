e6a90140b57b43f45f39e0863f3969c3bd56bd04
Ismail Sengul <ismail.sengul@saritay.com>
2023-03-06 13:45:44 +0300

    Longest Common Prefix implemented.

---
+++ src/Easy/LongestCommonPrefix/Solution.java
@@ -0,0 +1,29 @@
+package Easy.LongestCommonPrefix;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String[] strs = {"flower","flow","flight"};
+
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String result = "";
+        String current = strs[0];
+
+        for (int i = 0; i < current.length(); i++) {
+            for (int j = 0; j < strs.length; j++) {
+
+                if (strs[j].length() > i && current.charAt(i) == strs[j].charAt(i)) {
+                    if (j == strs.length-1) {
+                        result+=current.charAt(i);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
