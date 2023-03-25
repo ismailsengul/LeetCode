283750aff9e3bed10ab67396b161f0aa7a6cb356
Ismail Sengul <20200808603@ogr.akdeniz.edu.tr>
2023-03-26 02:22:24 +0300

    28. Find the Index of the First Occurrence in a String implemented.

---
+++ src/Easy/FindTheIndexOfTheFirstOccurrenceInAString/Solution.java
@@ -0,0 +1,15 @@
+package Easy.FindTheIndexOfTheFirstOccurrenceInAString;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String haystack = "leetcode";
+        String needle = "code";
+
+        System.out.println(strStr(haystack,needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
