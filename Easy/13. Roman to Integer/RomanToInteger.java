2d56a85371fb7c2b2b703e5e457b3856f4e54fe9
ismailsengul <ismailsengul64@gmail.com>
2023-03-27 23:09:08 +0300

    Romant to Integer implemented.

---
+++ Easy/13. Roman to Integer/RomanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+
+    }
+
+    public static  int romanToInt(String s) {
+        HashMap<Character,Integer> symbolValues = new HashMap<>();
+        int result = 0;
+
+        symbolValues.put('I',1);
+        symbolValues.put('V',5);
+        symbolValues.put('X',10);
+        symbolValues.put('L',50);
+        symbolValues.put('C',100);
+        symbolValues.put('D',500);
+        symbolValues.put('M',1000);
+
+        for(int i=0;i<s.length();i++){
+
+            if(i<s.length()-1 && symbolValues.get(s.charAt(i))<symbolValues.get(s.charAt(i+1))){
+                result+=symbolValues.get(s.charAt(i+1)) - symbolValues.get(s.charAt(i));
+                i++;
+            }else{
+                result+=symbolValues.get(s.charAt(i));
+            }
+        }
+
+        return result;
+
+    }
+}
