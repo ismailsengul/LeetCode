c59d18c3966909cf4724924fc7eaefd8b2e2ab2d
ismailsengul <ismailsengul64@gmail.com>
2023-03-27 17:00:08 +0300

    Length of Last Word implemented.

---
+++ src/Easy/LengthOfLastWord/Solution.java
@@ -0,0 +1,30 @@
+package Easy.LengthOfLastWord;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+
+    }
+
+    public static int lengthOfLastWord(String s) {
+
+        if (s.length() == 0) {
+            return 0;
+        }
+        int lastWordLength = 0;
+        int currentWordLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                currentWordLength++;
+                lastWordLength = currentWordLength;
+            }else {
+                currentWordLength = 0;
+            }
+
+        }
+
+        return lastWordLength;
+    }
+}
